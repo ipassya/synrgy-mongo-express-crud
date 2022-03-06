47aeb1aa40a8ba0fb6968b74c3e767283d86d5ba
Ihza Passya <ipassya@gmail.com>
2022-03-06 09:56:13 +0700

    Update connection database

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_CLIENT, {
+    const connect = await mongoose.connect(process.env.MONGODBCloud_CLIENT, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
